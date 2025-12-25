527f03768c8bd1a357e85adcfa6405186f4939c6
Adrian <bounce.adrian@gmail.com>
2025-12-25 22:14:40 +0100

    Add missing exports and adjust tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Renderer } from "./renderer.ts";
+export { Ctx } from "./ctx.ts";
 export {
 	vec2,
 	vec3,
@@ -9,7 +10,7 @@ export {
 	type Vec3,
 	type Vec4,
 } from "./utils.ts";
-export type { Sphere, Line } from "./shapeTypes.ts";
+export type { Sphere, Line, Text } from "./shapeTypes.ts";
 
 // TODO: Still wondering whether we should re-export THREE or not.
 export * as THREE from "three";
